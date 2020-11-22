e364efa2482b1a744131d6e9f198fcb5da200f55
Nirmala <nirmala_ira@hotmail.com>
2020-11-21 20:15:25 -0800

    changed relative path to absolute path

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,13 @@
 // required packages
 const fs = require("fs");
-const path = require("path");
-const db = require("../db/db.json");
+// const path = require("path");
+const resolve = require("path").resolve;
+const db = require(resolve("./db/db.json"));
+console.log(db);
+// const db = require("../db/db.json");
 const router = require("express").Router();
 console.log("this is api routes");
+// console.log(__dirname);
 
 //api call to display notes from db.json
 router.get("/notes", function (req, res) {
@@ -18,6 +22,8 @@ router.get("/notes", function (req, res) {
 //api call to save the data to db.json file
 router.post("/notes", function (req, res) {
      console.log("hi this is post");
+     // console.log(__dirname);
+     // console.log(__filename);
      let newNote = {
           id: Math.floor(Math.random() * 100),
           title: req.body.title,
@@ -25,18 +31,28 @@ router.post("/notes", function (req, res) {
      };
 
      //read db.json file
+     console.log("hi this is _dirname");
+     console.log(__dirname);
+     console.log(__filename);
+     console.log("./db/db.json");
      fs.readFile("./db/db.json", "utf8", (err, data) => {
+          // fs.readFile(db, "utf8", (err, data) => {
+          // fs.readFile(db, "utf8", (err, data) => {
           if (err) throw err;
           var dbdata = JSON.parse(data);
           dbdata.push(newNote);
 
-          fs.writeFileSync("./db/db.json", JSON.stringify(dbdata), function (
-               err
-          ) {
-               if (err) throw err;
-               console.log(dbdata);
-               res.send(newNote);
-          });
+          fs.writeFileSync(
+               "./db/db.json",
+               JSON.stringify(dbdata),
+               function (err) {
+                    // fs.writeFileSync(db, JSON.stringify(dbdata), function (err) {
+                    // fs.writeFileSync(db, JSON.stringify(dbdata), function (err) {
+                    if (err) throw err;
+                    console.log(dbdata);
+                    res.send(newNote);
+               }
+          );
      });
 });
 
@@ -48,13 +64,15 @@ router.delete("/notes/:id", function (req, res) {
           var dbdata = JSON.parse(data);
           var newNotes = dbdata.filter((note) => note.id != idTodelete);
 
-          fs.writeFileSync("./db/db.json", JSON.stringify(newNotes), function (
-               err
-          ) {
-               if (err) throw err;
-               console.log("note deleted");
-               res.send(db);
-          });
+          fs.writeFileSync(
+               "./db/db.json",
+               JSON.stringify(newNotes),
+               function (err) {
+                    if (err) throw err;
+                    console.log("note deleted");
+                    res.send(db);
+               }
+          );
      });
 });
 module.exports = router;
