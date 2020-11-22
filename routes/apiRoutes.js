6ac61d180e67c43ad1fcf07db5a002fe3d5c5ff0
Nirmala <nirmala_ira@hotmail.com>
2020-11-22 00:16:10 -0800

    changed absolute path

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,22 @@
 // required packages
 const fs = require("fs");
-// const path = require("path");
-const resolve = require("path").resolve;
-const db = require(resolve("./db/db.json"));
-console.log(db);
+const path = require("path");
+// const resolve = require("path").resolve;
+// const db = require(resolve("./db/db.json"));
+// console.log(db);
 // const db = require("../db/db.json");
+const db = require("../db/db.json");
 const router = require("express").Router();
 console.log("this is api routes");
+const db1 = path.join(__dirname, "..", "db", "db.json");
+console.log(db);
+console.log(db1);
 // console.log(__dirname);
+// const OUTPUT_DIR = path.resolve(__dirname, "./db");
+// console.log(OUTPUT_DIR);
+//creating  file in output folder
+// const outputPath = path.join(OUTPUT_DIR, "db.json");
+// console.log(outputPath);
 
 //api call to display notes from db.json
 router.get("/notes", function (req, res) {
@@ -31,48 +40,56 @@ router.post("/notes", function (req, res) {
      };
 
      //read db.json file
-     console.log("hi this is _dirname");
-     console.log(__dirname);
-     console.log(__filename);
-     console.log("./db/db.json");
-     // fs.readFile("./db/db.json", "utf8", (err, data) => {
-     // fs.readFile(db, "utf8", (err, data) => {
-     fs.readFile(`${__dirname}/../db/db.json`, "utf8", (err, data) => {
-          if (err) throw err;
-          var dbdata = JSON.parse(data);
-          dbdata.push(newNote);
 
-          fs.writeFileSync(
-               "${__dirname}/../db/db.json",
-               JSON.stringify(dbdata),
-               function (err) {
-                    // fs.writeFileSync(db, JSON.stringify(dbdata), function (err) {
-                    // fs.writeFileSync(db, JSON.stringify(dbdata), function (err) {
-                    if (err) throw err;
-                    console.log(dbdata);
-                    res.send(newNote);
-               }
-          );
-     });
+     // fs.readFile("./db/db.json", "utf8", (err, data) => {
+     // fs.readFile("./db/db.json", "utf8", (err, data) => {
+     fs.readFile(
+          path.join(__dirname, "..", "db", "db.json"),
+          "utf8",
+          (err, data) => {
+               if (err) throw err;
+               var dbdata = JSON.parse(data);
+               dbdata.push(newNote);
+
+               // fs.writeFileSync(
+               // "${__dirname}/../db/db.json",
+               // JSON.stringify(dbdata),
+               // function (err) {
+               // fs.writeFileSync(db, JSON.stringify(dbdata), function (err) {
+               fs.writeFileSync(
+                    path.join(__dirname, "..", "db", "db.json"),
+                    JSON.stringify(dbdata),
+                    function (err) {
+                         if (err) throw err;
+                         console.log(dbdata);
+                         res.send(newNote);
+                    }
+               );
+          }
+     );
 });
 
 //api call to delete item from db.json file
 router.delete("/notes/:id", function (req, res) {
      let idTodelete = req.params.id;
-     fs.readFile(`${__dirname}/../db/db.json`, "utf8", (err, data) => {
-          if (err) throw err;
-          var dbdata = JSON.parse(data);
-          var newNotes = dbdata.filter((note) => note.id != idTodelete);
+     fs.readFile(
+          path.join(__dirname, "..", "db", "db.json"),
+          "utf8",
+          (err, data) => {
+               if (err) throw err;
+               var dbdata = JSON.parse(data);
+               var newNotes = dbdata.filter((note) => note.id != idTodelete);
 
-          fs.writeFileSync(
-               "${__dirname}/../db/db.json",
-               JSON.stringify(newNotes),
-               function (err) {
-                    if (err) throw err;
-                    console.log("note deleted");
-                    res.send(db);
-               }
-          );
-     });
+               fs.writeFileSync(
+                    path.join(__dirname, "..", "db", "db.json"),
+                    JSON.stringify(newNotes),
+                    function (err) {
+                         if (err) throw err;
+                         console.log("note deleted");
+                         res.send(db);
+                    }
+               );
+          }
+     );
 });
 module.exports = router;
