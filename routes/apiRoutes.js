e1e91efb25e900ec4421d07bad0310e6a76b0457
Nirmala <nirmala_ira@hotmail.com>
2020-12-01 22:08:27 -0800

    changes in api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,56 +3,76 @@ const fs = require("fs");
 const path = require("path");
 const db = require("../db/db.json");
 const router = require("express").Router();
-var timeout = require("connect-timeout");
+// var timeout = require("connect-timeout");
 console.log("this is api routes");
-const db1 = path.join(__dirname, "..", "db", "db.json");
-console.log(db);
-console.log(db1);
-
+// const db1 = path.join(__dirname, "..", "db", "db.json");
+// const path = require("path");
+// console.log(db);
+// console.log(db1);
+const jsondb = require("../db/db");
 router.get("/notes", function (req, res) {
      console.log(db);
      var count = Object.keys(db).length;
      console.log(count);
      if (count == 0)
           res.send("<html><body><h1>No Saved Notes </h1></body></html>");
-     else res.json(db);
+     else res.json(jsondb);
 });
 
-//api call to save the data to db.json file
+//new
+
 router.post("/notes", function (req, res) {
-     console.log("hi this is post");
-     var dbdata;
-     console.log(__dirname);
-     console.log(__dirname, "../db/db.json");
      let newNote = {
           id: Math.floor(Math.random() * 100),
           title: req.body.title,
           text: req.body.text,
      };
-     console.log("outside");
-     fs.readFile("./db/db.json", "utf8", (err, data) => {
-          if (err) throw err;
-          else {
-               console.log("1");
-               dbdata = JSON.parse(data);
-               dbdata.push(newNote);
+     jsondb.push(newNote);
+     fs.writeFile(
+          path.join(__dirname, "../db/db.json"),
+          JSON.stringify(jsondb, null, 2),
+          (err) => {
+               if (err) throw err;
           }
-          console.log("2");
-          fs.writeFileSync(
-               "./db/db.json",
-               JSON.stringify(dbdata),
-               function (err) {
-                    if (err) throw err;
-                    else {
-                         console.log("3");
-                         console.log(dbdata);
-
-                         res.status(200).end();
-                    }
-               }
-          );
-     });
+     );
+     res.json(newNote);
 });
+
+//api call to save the data to db.json file
+// router.post("/notes", function (req, res) {
+//      console.log("hi this is post");
+//      var dbdata;
+//      console.log(__dirname);
+//      console.log(__dirname, "../db/db.json");
+//      let newNote = {
+//           id: Math.floor(Math.random() * 100),
+//           title: req.body.title,
+//           text: req.body.text,
+//      };
+//      console.log("outside");
+//      fs.readFile("./db/db.json", "utf8", (err, data) => {
+//           if (err) throw err;
+//           else {
+//                console.log("1");
+//                dbdata = JSON.parse(data);
+//                dbdata.push(newNote);
+//           }
+//           console.log("2");
+//           fs.writeFileSync(
+//                "./db/db.json",
+//                JSON.stringify(dbdata),
+//                function (err) {
+//                     if (err) throw err;
+//                     else {
+//                          console.log("3");
+//                          console.log(dbdata);
+
+//                          res.status(200).end();
+//                     }
+//                }
+//           );
+//      });
+// });
 
 //api call to delete item from db.json file
 router.delete("/notes/:id", function (req, res) {
