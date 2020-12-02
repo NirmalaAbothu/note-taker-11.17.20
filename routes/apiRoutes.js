b5cac0469474b95e87eb632bafe4258f0a555f1a
Nirmala <nirmala_ira@hotmail.com>
2020-12-02 00:15:49 -0800

    deleted commented code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,12 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const db = require("../db/db.json");
 const router = require("express").Router();
-// var timeout = require("connect-timeout");
 console.log("this is api routes");
-// const db1 = path.join(__dirname, "..", "db", "db.json");
-// const path = require("path");
-// console.log(db);
-// console.log(db1);
 const jsondb = require("../db/db");
 router.get("/notes", function (req, res) {
      console.log(db);
@@ -19,7 +14,7 @@ router.get("/notes", function (req, res) {
      else res.json(jsondb);
 });
 
-//new
+//api call to add notes
 
 router.post("/notes", function (req, res) {
      let newNote = {
@@ -37,42 +32,6 @@ router.post("/notes", function (req, res) {
      );
      res.json(newNote);
 });
-
-//api call to save the data to db.json file
-// router.post("/notes", function (req, res) {
-//      console.log("hi this is post");
-//      var dbdata;
-//      console.log(__dirname);
-//      console.log(__dirname, "../db/db.json");
-//      let newNote = {
-//           id: Math.floor(Math.random() * 100),
-//           title: req.body.title,
-//           text: req.body.text,
-//      };
-//      console.log("outside");
-//      fs.readFile("./db/db.json", "utf8", (err, data) => {
-//           if (err) throw err;
-//           else {
-//                console.log("1");
-//                dbdata = JSON.parse(data);
-//                dbdata.push(newNote);
-//           }
-//           console.log("2");
-//           fs.writeFileSync(
-//                "./db/db.json",
-//                JSON.stringify(dbdata),
-//                function (err) {
-//                     if (err) throw err;
-//                     else {
-//                          console.log("3");
-//                          console.log(dbdata);
-
-//                          res.status(200).end();
-//                     }
-//                }
-//           );
-//      });
-// });
 
 //api call to delete item from db.json file
 router.delete("/notes/:id", function (req, res) {
@@ -99,23 +58,6 @@ router.delete("/notes/:id", function (req, res) {
           }
      );
      res.json(idTodelete);
-
-     // fs.readFile("./db/db.json", "utf8", (err, data) => {
-     //      if (err) throw err;
-     //      var dbdata = JSON.parse(data);
-     //      var newNotes = dbdata.filter((note) => note.id != idTodelete);
-
-     //      fs.writeFileSync(
-     //           "./db/db.json",
-     //           JSON.stringify(newNotes),
-     //           function (err) {
-     //                if (err) throw err;
-     //                console.log("note deleted");
-
-     //                res.status(200).end();
-     //           }
-     //      );
-     // });
 });
 
 module.exports = router;
